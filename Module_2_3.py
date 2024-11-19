0d9a57773f05a59e5f3396417fc88daabd4500ed
SSasha88 <d1rus1853@gmail.com>
2024-11-19 12:59:19 +0300

    Добавил 2 решение. Домашняя работа по уроку "Стиль кода часть II. Цикл While"

--- Module_2_3.py
+++ Module_2_3.py
@@ -1,3 +1,4 @@
+print('вариант 1')
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 c = len(my_list)
 # b = my_list
@@ -9,3 +10,16 @@ while c > 0:
             break
         continue
     break
+print('вариант 2')
+my_list1 = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+f = 0
+while f < len(my_list1):
+    if my_list1[f] > 0:
+        print(my_list1[f])
+        f += 1
+    elif my_list1[f] == 0:
+        f += 1
+        continue
+    else:
+        break
+
